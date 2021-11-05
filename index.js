741743fd70ce2a00bbdc079f76ec4e2cb89887e6
Todd Demone <tdemone1@gmail.com>
2021-11-05 19:59:44 +0000

    complete 'sum from a to b recursively'

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
-
-function sum(fromN, toN) {
+const sum = function(fromN, toN) {
   // Sum all the values from fromN up to toN
-}
+  // The base case is where fromN === toN
+  if (fromN === toN) {
+    return toN;
+  }
+  // The recursive case is where fromN < toN
+  return fromN + sum((fromN + 1), toN);
+};
 
 module.exports = sum;
